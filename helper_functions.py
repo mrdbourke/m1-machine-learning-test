ad7bfb12dff20aa5bc0e7ff5f2167038245e31c5
mrdbourke <danielbourke@icloud.com>
2021-11-14 16:28:58 +1000

    add helper functions

---
+++ helper_functions.py
@@ -0,0 +1,17 @@
+from timeit import default_timer as timer 
+def print_train_time(start, end, device=None):
+    """Prints difference between start and end time.
+
+    Args:
+        start (float): Start time of computation (preferred in timeit format). 
+        end (float): End time of computation.
+
+    Returns:
+        float: time between start and end in seconds (higher is longer).
+    """
+    total_time = end - start
+    if device:  
+        print(f"\nTrain time on {device}: {total_time:.3f} seconds")
+    else:
+        print(f"\nTrain time: {total_time:.3f} seconds")
+    return total_time
